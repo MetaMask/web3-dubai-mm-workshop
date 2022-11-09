1f4bd35b99cc0fea701e778382c5d5c49344a235
GuiBibeau <g.bibeaulaviolette@gmail.com>
2022-11-09 19:36:51 +0200

    fix transpiling issues

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,3 +1,8 @@
-module.exports = {
+const withTM = require("next-transpile-modules")(["blockchain"]); // pass the modules you would like to see transpiled
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
 };
+
+module.exports = withTM(nextConfig);
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,6 +1,7 @@
-import { Hello_factory } from "blockchain";
+import { Hello__factory } from "blockchain";
 
 export default function Web() {
+  const helloContract = new Hello__factory();
   return (
     <div>
       <h1>Web</h1>
