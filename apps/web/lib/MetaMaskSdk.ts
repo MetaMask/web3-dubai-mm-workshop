dc1b1ba86adb5ac48142c3059f67b53cca3f8d89
GuiBibeau <g.bibeaulaviolette@gmail.com>
2022-11-10 17:07:52 +0200

    ethers and extension

---
+++ apps/web/lib/MetaMaskSdk.ts
@@ -0,0 +1,12 @@
+import MetaMaskSDK from "@metamask/sdk";
+import { ethers } from "ethers";
+
+export const getProvider = () => {
+  if (typeof window === undefined) {
+    return null;
+  }
+
+  const MMSDK = new MetaMaskSDK();
+
+  return MMSDK.getProvider() as ethers.providers.ExternalProvider;
+};
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,7 +1,16 @@
+import { getProvider } from "../lib/MetaMaskSdk";
+
 export default function Web() {
+  const handleConnect = () => {
+    const provider = getProvider();
+    if (typeof provider !== undefined) {
+      provider.request({ method: "eth_requestAccounts" });
+    }
+  };
   return (
     <div>
       <h1>Web</h1>
+      <button onClick={handleConnect}>connect</button>
     </div>
   );
 }
