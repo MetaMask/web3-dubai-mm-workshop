fd230e1dc9f6fc1874272945abc13c3e523c59c7
GuiBibeau <g.bibeaulaviolette@gmail.com>
2022-11-10 18:34:54 +0200

    typechain

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,11 +1,27 @@
 import { getProvider } from "../lib/MetaMaskSdk";
+import { ETHTickets__factory } from "blockchain";
+import { config } from "../lib/config";
+import { ethers } from "ethers";
 
 export default function Web() {
   const handleConnect = () => {
     const provider = getProvider();
-    if (typeof provider !== undefined) {
-      provider.request({ method: "eth_requestAccounts" });
+    if (typeof provider?.request !== undefined) {
+      provider?.request({ method: "eth_requestAccounts" });
     }
+  };
+
+  const handleMint = () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const contract = ETHTickets__factory.connect(
+      config.contractAddress,
+      signer
+    );
+    contract
+      .mintNFT
+      // args here
+      ();
   };
   return (
     <div>
