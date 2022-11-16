c50d472d61aaca15b92e45cc93a40609e89497ec
Eric Bishard <eric@httpjunkie.com>
2022-11-16 18:31:05 +0400

    Add comment to Tickets.tsx

--- apps/web/components/tickets/Tickets.tsx
+++ apps/web/components/tickets/Tickets.tsx
@@ -35,6 +35,10 @@ const TicketCategoryDetail: React.FC<Ticket> = ({
     setIsMinting(true);
 
     const provider = new ethers.providers.Web3Provider(window.ethereum);
+    // In ethers.js, providers allow you to query data from the blockchain. 
+    // They represent the way you connect to the blockchain. With them you can only call view methods on contracts and get data from those contract.
+    // Signers are backed by providers and have access to an ethereum account. 
+    // Therefore, they can sign transactions that modify the state of the blockchain by storing or changing information.
     const signer = provider.getSigner();
 
     const factory = new ETHTickets__factory(signer);
@@ -62,7 +66,7 @@ const TicketCategoryDetail: React.FC<Ticket> = ({
     <FlexItem>
       <TicketType>
         <TicketTypeText>{event}</TicketTypeText>
-        <p>{description}</p>
+        <p>{description} ({price} ETH)</p>
         <Button disabled={isMinting} onClick={mintTicket}>
           <SiEthereum /> {isMinting ? 'Minting...' : 'Mint'} Ticket
         </Button>
